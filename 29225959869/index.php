65c131c41e657a3add40d0e73e3ab27dd1f09f42
Valdir Lourenco <valdir2310@yahoo.com.br>
2022-03-26 16:41:20 -0300

    estrutura basica da LP

--- 29225959869/index.php
+++ 29225959869/index.php
@@ -47,7 +47,8 @@
 <body>
 
 <main>
-    <div class="container">
+<!--    <div class="container">-->
+<!--        TIMER-->
         <div class="row">
             <div class="col">
                 Essa oferta acaba em:
@@ -59,9 +60,201 @@
                 Ou enquanto durarem os estoques
             </div>
         </div>
-    </div>
+<!--        HEADER-->
+        <div class="row">
+            <div class="col">
+                Essa oferta acaba em:
+            </div>
+            <div class="col">
+                <div id="timer"></div>
+            </div>
+        </div>
+<!--        LIVRO HEADER-->
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <div class="col">Não existe marketing sem uma história de amor.</div>
+                    <div class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                    <div class="col">
+                        <button class="btn btn-primary">COMPRE AGORA</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <img src="images/livro-header.png">
+            </div>
+        </div>
+<!--        VIDEO-->
+        <div class="row">
+            <div class="col">
+                <iframe src="https://player.vimeo.com/video/691105207?h=2c73f1c2f5" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            </div>
+        </div>
+<!--        LIVRO 1-->
+        <div class="row">
+            <div class="col-6">
+                <img src="images/livro-01.png" class="img-fluid">
+            </div>
+            <div class="col-6">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+            </div>
+        </div>
+<!--        LIVRO 2-->
+        <div class="row">
+            <div class="col">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+            </div>
+            <div class="col">
+                <img src="images/livro-01.png">
+            </div>
+        </div>
+<!--        BERNADETE-->
+        <div class="row">
+            <div class="col">
+                <img src="images/bernadete.png">
+            </div>
+            <div class="col">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+            </div>
+        </div>
+<!--        GALERIA-->
+        <div class="row">
+            <div class="col">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+            </div>
+            <div class="col">
+                GALERIA
+            </div>
+        </div>
+<!--        TEXTO-->
+        <div class="row">
+            <div class="col">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+            </div>
+        </div>
+<!--        SLIDES-->
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+            </div>
+        </div>
+<!--        PRECO-->
+        <div class="row">
+            <div class="col">
+                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+                <div>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</div>
+
+            </div>
+        </div>
+<!--        ACTION-->
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-primary">COMPRE AGORA</button>
+            </div>
+        </div>
+<!--        DEPOIMENTOS-->
+        <div class="row">
+            <div class="col">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!--        FAQ-->
+        <div class="row">
+            <div class="col">
+                <h3>Perguntas frequentes</h3>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Accordion Item #1
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Accordion Item #2
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Accordion Item #3
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!--    </div>-->
 
 </main>
+<footer>
+    <div class="row">
+        <div class="col">
+            <img src="images/logo-footer.png">
+        </div>
+    </div>
+</footer>
 
 
 <script src="bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
